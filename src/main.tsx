ae8341111ba735b26687e610ec76453a575187b3
Awin Gray <grayawin@gmail.com>
2025-10-05 19:59:18 +0700

    added tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,11 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
-    
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-    <QueryClientProvider client={queryClient}>
-		  <App />
-    </QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</StrictMode>,
 );
