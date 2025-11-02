59b1b3844c3beae580b41f56f08f0cf4d3233244
Awin Gray <grayawin@gmail.com>
2025-11-02 19:09:02 +0700

    redirect home page to events if authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { ROUTES } from "@/constants/routes";
 import LandingComponent from "@/pages/landing";
+import { useAuthStore } from "@/stores/auth-store";
 
 export const Route = createFileRoute(ROUTES.HOME)({
+	beforeLoad: () => {
+		const { isAuthenticated } = useAuthStore.getState();
+		if (isAuthenticated)
+			throw redirect({ to: ROUTES.EVENTS });
+	},
 	component: LandingComponent,
 });
