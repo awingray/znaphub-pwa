f7c75826edb94c7b380f154b3599fb5f97c9cfdf
Awin Gray <grayawin@gmail.com>
2025-11-09 14:10:16 +0700

    allow null forgiving operator

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,10 +1,19 @@
-import { useCreateEvent } from "@/api/events/mutations";
 import { EventListQueryOptions } from "@/api/events/queries";
-import { Button } from "@/components/ui/button";
+import Each from "@/components/flow/each";
+import { Show } from "@/components/flow/show";
 import { useQuery } from "@tanstack/react-query";
+import EventForm from "./forms/event-form";
 
 export default function EventListComponent() {
 	const { data } = useQuery(EventListQueryOptions());
 
-	return <div>Hello "/_auth/events"!</div>;
+	return (
+		<div>
+			Hello "/_auth/events"!
+			<Show.When condition={!!data && data.length > 0}>
+				<Each of={data!} render={(event) => event.name} />
+			</Show.When>
+			<EventForm />
+		</div>
+	);
 }
